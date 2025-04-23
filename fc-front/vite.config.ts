ecba4ce1db9057627a5222bde82f75bcacc307a5
hong-tm <asuszen48319@gmail.com>
2025-04-23 21:58:22 +0800

    feat: update Vite configuration

--- fc-front/vite.config.ts
+++ fc-front/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		// visualizer({
+		// 	open: true,
+		// 	gzipSize: true,
+		// 	filename: "stats.html",
+		// }) as PluginOption,
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
