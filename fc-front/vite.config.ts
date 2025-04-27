a7e46165cd1f1d0b13bd1c7ff97bcbef84fe29bd
hong-tm <asuszen48319@gmail.com>
2025-04-28 03:17:09 +0800

    refactor: update Vite configuration to use new plugin imports and enhance build settings

--- fc-front/vite.config.ts
+++ fc-front/vite.config.ts
@@ -1,29 +1,28 @@
 import { defineConfig } from "vite";
-import build from "@hono/vite-build/node";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import viteReact from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import type { PluginOption } from "vite";
 // import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		react(),
-		build({
-			// Defaults are `src/index.ts`,`./src/index.tsx`,`./app/server.ts`
-			entry: "./src/index.tsx",
-			// port option is only for Node.js adapter. Default is 3000
-			port: 3000,
-		}),
+		viteReact(),
 		// visualizer({
 		// 	open: true,
 		// 	gzipSize: true,
 		// 	filename: "stats.html",
 		// }) as PluginOption,
-	],
+	] as PluginOption[],
+	build: {
+		minify: false, // 禁用压缩，减少内存使用
+		target: "esnext",
+		chunkSizeWarningLimit: 1000, // 增加警告的 chunk 大小限制
+	},
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "./src"),
-			"@server": path.resolve(__dirname, "../fc-back"),
+			"@": resolve(__dirname, "./src"),
+			"@server": resolve(__dirname, "../fc-back"),
 		},
 	},
 	css: {
