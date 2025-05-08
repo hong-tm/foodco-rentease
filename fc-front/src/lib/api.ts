3afbbf5162918ea4f1b2ddb56c0bd43f30b7fa85
hong-tm <asuszen48319@gmail.com>
2025-05-08 23:22:43 +0800

    feat: enhance API type definitions and improve code formatting

--- fc-front/src/lib/api.ts
+++ fc-front/src/lib/api.ts
@@ -1,6 +1,25 @@
-import { hc } from "hono/client";
-import { type ApiRoutes } from "@server/index";
+import { hc } from 'hono/client'
+import { type ApiRoutes } from '@server/index'
 
-const client = hc<ApiRoutes>("/");
+const client = hc<ApiRoutes>('/')
 
-export const api = client.api;
+export const api = client.api
+
+// this is a trick to calculate the type when compiling
+// const api = hc<ApiRoutes>('')
+// export type Api = typeof api
+
+// export const apiWithType = (...args: Parameters<typeof hc>): Api =>
+//   hc<ApiRoutes>(...args)
+
+export type ErrorSchema = {
+  error: {
+    issues: {
+      code: string
+      path: (string | number)[]
+      message?: string | undefined
+    }[]
+    name: string
+  }
+  success: boolean
+}
