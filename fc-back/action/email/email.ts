bfd5f04faf433271b55bce2b231476a6321e3728
YiHong <asuszen48319@gmail.com>
2025-07-17 03:13:48 +0800

    chore: update resend function

--- fc-back/action/email/email.ts
+++ fc-back/action/email/email.ts
@@ -1,9 +1,6 @@
-import * as dotenv from 'dotenv'
 import { Resend } from 'resend'
 
 import env from './../../env.js'
-
-dotenv.config()
 
 export async function sendEmail({
   to,
@@ -14,27 +11,22 @@ export async function sendEmail({
   subject: string
   text: string
 }) {
-  try {
-    const resend: any = new Resend(env.RESEND_API)
+  const resend = new Resend(env.RESEND_API)
 
-    if (!env.RESEND_API) {
-      throw new Error('RESEND_API is not set')
-    }
-
-    const { data, error } = await resend.emails.send({
-      from: `tech@nullsoul.com`,
-      to: to,
-      subject: subject,
-      text: text,
-    })
-
-    if (error) {
-      throw new Error(error.message)
-    }
-
-    return data
-  } catch (err) {
-    console.log(err)
-    throw err
+  if (!env.RESEND_API) {
+    throw new Error('RESEND_API is not set')
   }
+
+  const { data, error } = await resend.emails.send({
+    from: `tech@nullsoul.com`,
+    to: to,
+    subject: subject,
+    text: text,
+  })
+
+  if (error) {
+    throw new Error(error.name)
+  }
+
+  return data
 }
